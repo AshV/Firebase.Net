4e952ab3ebbcd5f7cbf54e066f2c879e25c475c1
Ashish Vishwakarma <Ashish6318@gmail.com>
2017-07-06 15:40:03 +0530

    Added Sample Project

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Firebase.Net
+namespace Sample
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
