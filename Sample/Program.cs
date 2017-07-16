f95cd4cc9cfa622fbbb9fce0895c59025acefa83
AshV <ashish6318@gmail.com>
2017-07-16 18:41:57 +0530

    Synchrons is working fine, Adding JSON validtion and Async Now

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Console;
 using Firebase.Net;
+using Newtonsoft.Json.Linq;
 
 namespace Sample
 {
@@ -9,14 +10,42 @@ namespace Sample
         {
             FirebaseDB DB = new FirebaseDB("https://c-sharpcorner-2d7ae.firebaseio.com");
 
-            var data = "{\"Team-Awesome\":{\"Members\":{\"M1\":{\"City\":\"Hyderabad\",\"Name\":\"Ashish\"},\"M2\":{\"City\":\"Cyberabad\",\"Name\":\"Vivek\"},\"M3\":{\"City\":\"Secunderabad\",\"Name\":\"Pradeep\"}}}}";
+            var data = "{{\"Tea.m-Awesome\":{\"Members\":{\"M1\":{\"City\":\"Hyderabad\",\"Name\":\"Ashish\"},\"M2\":{\"City\":\"Cyberabad\",\"Name\":\"Vivek\"},\"M3\":{\"City\":\"Secunderabad\",\"Name\":\"Pradeep\"}}}}";
+
+            var dt = @"{
+  'Team-Awesome': {
+                'Members': {
+                    'M1': {
+                        'City': 'Hyderabad',
+        'Name': 'Ashish'
+                    },
+      'M2': {
+                        'City': 'Cyberabad',
+        'Nam.e': 'Vivek'
+      },
+      'M3': {
+                        'City': 'Secunderabad',
+        'Name': 'Pradeep'
+      }
+                }
+            }
+        }
+";
+
+            var d = dt;
+
+
+         var jt=   JToken.Parse(data);
+            var j = jt.ToString();
+
+            WriteLine(j);
 
             var get = DB.Get();
             Write(get.Success);
             Write(get.JSONContent);
             WriteLine();
 
-            var put = DB.Put(data);
+            var put = DB.Put(j);
             Write(put.Success);
 
             var post = DB.Post(data);
