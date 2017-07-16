f8c66061ce362e42f841a4ad4d0f51ac0ac2c735
AshV <ashish6318@gmail.com>
2017-07-17 01:56:50 +0530

    Moved some code to UtilityHelper.cs

--- FirebaseNet/Database/FirebaseRequest.cs
+++ FirebaseNet/Database/FirebaseRequest.cs
@@ -8,24 +8,16 @@ namespace FirebaseNet.Database
 {
     using System;
     using System.Net.Http;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// Firebase Request
     /// </summary>
-    public class FirebaseRequest
+    class FirebaseRequest
     {
         /// <summary>
         /// Suffix to be added in each resource URI
         /// </summary>
         private const string JSON_SUFFIX = ".json";
-
-        /// <summary>
-        /// User Agent Header in HTTP Request
-        /// </summary>
-        private const string USER_AGENT = "firebase-net/1.0";
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FirebaseRequest"/> class
@@ -69,7 +61,7 @@ namespace FirebaseNet.Database
         public FirebaseResponse Execute()
         {
             Uri requestURI;
-            if (this.ValidateURI(this.Uri))
+            if (UtilityHelper.ValidateURI(this.Uri))
             {
                 requestURI = new Uri(this.Uri);
             }
@@ -81,13 +73,13 @@ namespace FirebaseNet.Database
             string json = null;
             if (this.JSON != null)
             {
-                if (!this.TryParseJSON(this.JSON, Method, out json))
+                if (!UtilityHelper.TryParseJSON(this.JSON, out json))
                 {
                     return new FirebaseResponse(false, string.Format("Invalid JSON : {0}", json));
                 }
             }
 
-            var response = this.RequestHelper(this.Method, requestURI, json);
+            var response = UtilityHelper.RequestHelper(this.Method, requestURI, json);
             response.Wait();
             var result = response.Result;
 
@@ -106,82 +98,6 @@ namespace FirebaseNet.Database
             }
 
             return firebaseResponse;
-        }
-
-        /// <summary>
-        /// Validates a URI
-        /// </summary>
-        /// <param name="url">URI as string</param>
-        /// <returns>True if valid</returns>
-        private bool ValidateURI(string url)
-        {
-            Uri locurl;
-            if (System.Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out locurl))
-            {
-                if (
-                    !(locurl.IsAbsoluteUri &&
-                      (locurl.Scheme == "http" || locurl.Scheme == "https")) ||
-                    !locurl.IsAbsoluteUri)
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Validates JSON string
-        /// </summary>
-        /// <param name="inJSON">JSON to be validatedd</param>
-        /// <param name="output">Valid JSON or Error Message</param>
-        /// <returns>True if valid</returns>
-        private bool TryParseJSON(string inJSON, HttpMethod method, out string output)
-        {
-            try
-            {
-                JToken parsedJSON = JToken.Parse(inJSON);
-                output = parsedJSON.ToString();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("Expected ':'") && method.Equals(new HttpMethod("PATCH")))
-                {
-                    output = inJSON;
-                    return true;
-                }
-
-                output = ex.Message;
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Makes Asynchronus HTTP requests
-        /// </summary>
-        /// <param name="method">HTTP method</param>
-        /// <param name="uri">URI of resource</param>
-        /// <param name="json">JSON string</param>
-        /// <returns>HTTP Responce as Task</returns>
-        private Task<HttpResponseMessage> RequestHelper(HttpMethod method, Uri uri, string json = null)
-        {
-            var client = new HttpClient();
-            var msg = new HttpRequestMessage(method, uri);
-            msg.Headers.Add("user-agent", USER_AGENT);
-            if (json != null)
-            {
-                msg.Content = new StringContent(
-                    json,
-                    UnicodeEncoding.UTF8,
-                    "application/json");
-            }
-
-            return client.SendAsync(msg);
         }
     }
 }
---
+++ FirebaseNet/Database/UtilityHelper.cs
@@ -0,0 +1,95 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UtilityHelper.cs" company="AshishVishwakarma.com">
+// Github/AshV
+// </copyright>
+// <author>Ashish Vishwakarma</author>
+//-----------------------------------------------------------------------
+namespace FirebaseNet.Database
+{
+    using System;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Utility Helper Class
+    /// </summary>
+    class UtilityHelper
+    {
+        /// <summary>
+        /// User Agent Header in HTTP Request
+        /// </summary>
+        private const string USER_AGENT = "firebase-net/1.0";
+
+        /// <summary>
+        /// Validates a URI
+        /// </summary>
+        /// <param name="url">URI as string</param>
+        /// <returns>True if valid</returns>
+        public static bool ValidateURI(string url)
+        {
+            Uri locurl;
+            if (System.Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out locurl))
+            {
+                if (
+                    !(locurl.IsAbsoluteUri &&
+                      (locurl.Scheme == "http" || locurl.Scheme == "https")) ||
+                    !locurl.IsAbsoluteUri)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validates JSON string
+        /// </summary>
+        /// <param name="inJSON">JSON to be validatedd</param>
+        /// <param name="output">Valid JSON or Error Message</param>
+        /// <returns>True if valid</returns>
+        public static bool TryParseJSON(string inJSON, out string output)
+        {
+            try
+            {
+                JToken parsedJSON = JToken.Parse(inJSON);
+                output = parsedJSON.ToString();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Makes Asynchronus HTTP requests
+        /// </summary>
+        /// <param name="method">HTTP method</param>
+        /// <param name="uri">URI of resource</param>
+        /// <param name="json">JSON string</param>
+        /// <returns>HTTP Responce as Task</returns>
+        public static Task<HttpResponseMessage> RequestHelper(HttpMethod method, Uri uri, string json = null)
+        {
+            var client = new HttpClient();
+            var msg = new HttpRequestMessage(method, uri);
+            msg.Headers.Add("user-agent", USER_AGENT);
+            if (json != null)
+            {
+                msg.Content = new StringContent(
+                    json,
+                    UnicodeEncoding.UTF8,
+                    "application/json");
+            }
+
+            return client.SendAsync(msg);
+        }
+    }
+}
